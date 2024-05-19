aec7c08f3e7c0baaa23c203d1316e5e2b4c0791d
pro-masud <promasudbd@gmail.com>
2024-05-19 17:56:28 +0600

    block plugin svg icon how to add and working succesfully

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0755a090e7676ec2a308');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7ea0524f2b451692cd44');
